761697613900063c1a7dd13a628725882d31ace7
julrich <jochenulrich@t-online.de>
2012-09-13 14:56:51 +0200

    Removes dependencies to jquery 1.7.0+ in unit tests
    * Replaces .off() calls with .die() to remove dependency to jquery 1.7.0+ in unit tests

--- tests/test.js
+++ tests/test.js
@@ -83,7 +83,7 @@ $(document).ready(function() {
 	
 	module('event tests', {
 		teardown: function() {
-			$('#qunit-fixture').off();
+			$('#qunit-fixture').die();
 		}
 	});
 	test("event triggering", function() {
